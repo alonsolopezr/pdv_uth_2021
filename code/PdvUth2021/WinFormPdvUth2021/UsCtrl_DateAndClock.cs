f9037b20750c2bd2a523d6a0450f3e6899ae6429
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2021-05-01 15:23:26 -0700

    Creating user control for CLOCK, and ignoring .suo files
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

---
+++ code/PdvUth2021/WinFormPdvUth2021/UsCtrl_DateAndClock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormPdvUth2021
+{
+    public partial class UsCtrl_DateAndClock : UserControl
+    {
+        public UsCtrl_DateAndClock()
+        {
+            InitializeComponent();
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void timerDate_Tick(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+        }
+    }
+}
